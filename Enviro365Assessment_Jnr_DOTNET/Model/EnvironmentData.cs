00e6bd0ba268f7119931d1333611d964137c8110
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-20 19:15:04 +0200

    Implement the data model

--- Enviro365Assessment_Jnr_DOTNET/Model/EnvironmentData.cs
+++ Enviro365Assessment_Jnr_DOTNET/Model/EnvironmentData.cs
@@ -1,6 +1,16 @@
-﻿namespace Enviro365Assessment_Jnr_DOTNET.Model;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Enviro365Assessment_Jnr_DOTNET.Model;
+
+[Table("EnvironmentData")]
 public class EnvironmentData
 {
-    
+    [Key] [Column("EnvironmentDataId")] public required int Id { get; set; }
+
+    [Column("File_name")] public required string FileName { get; set; }
+
+    [Column("Upload_Date")] public required DateTime UploadDate { get; set; }
+
+    [Column("Processed_Data")] public required string ProcessedData { get; set; }
 }
