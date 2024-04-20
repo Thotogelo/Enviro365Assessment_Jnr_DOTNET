decda60ee8c7f4a0d481158d930570916e71c1f3
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-20 16:29:07 +0200

    Config Swagger UI

--- Enviro365Assessment_Jnr_DOTNET/Program.cs
+++ Enviro365Assessment_Jnr_DOTNET/Program.cs
@@ -7,8 +7,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(opt =>
+{
+    opt.DocumentTitle = "Enviro365Assessment_Jnr_DOTNET";
+    opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Enviro365Assessment_Jnr_DOTNET");
+    opt.RoutePrefix = string.Empty;
+});
 
 app.MapGet("/test", () => "Hello World!");
-
 app.Run();
