b2797edfacb5e870f1361ab86f812942410918ff
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-21 13:57:43 +0200

    Migrations initial commit

---
+++ Enviro365Assessment_Jnr_DOTNET/Migrations/20240421115605_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Enviro365Assessment_Jnr_DOTNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnvironmentData",
+                columns: table => new
+                {
+                    EnvironmentDataId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    File_name = table.Column<string>(type: "text", nullable: false),
+                    Upload_Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Processed_Data = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnvironmentData", x => x.EnvironmentDataId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnvironmentData");
+        }
+    }
+}
