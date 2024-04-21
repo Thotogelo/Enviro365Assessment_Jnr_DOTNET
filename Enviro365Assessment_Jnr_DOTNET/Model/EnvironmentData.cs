a22939d3e92bbdc1519664b95291ffec9294355f
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-21 12:05:48 +0200

    Improve model

--- Enviro365Assessment_Jnr_DOTNET/Model/EnvironmentData.cs
+++ Enviro365Assessment_Jnr_DOTNET/Model/EnvironmentData.cs
@@ -3,14 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Enviro365Assessment_Jnr_DOTNET.Model;
 
-[Table("EnvironmentData")]
 public class EnvironmentData
 {
-    [Key] [Column("EnvironmentDataId")] public required int Id { get; set; }
+    public required int Id { get; set; }
 
-    [Column("File_name")] public required string FileName { get; set; }
+    public required string FileName { get; set; }
 
-    [Column("Upload_Date")] public required DateTime UploadDate { get; set; }
+    public required DateTime UploadDate { get; set; }
 
-    [Column("Processed_Data")] public required string ProcessedData { get; set; }
+    public required string ProcessedData { get; set; }
 }
