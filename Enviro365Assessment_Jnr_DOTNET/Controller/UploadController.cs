728e1aa745c0b42eb9ba1d8731a84eba65bef17f
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-23 01:36:22 +0200

    Add Controller templates

--- Enviro365Assessment_Jnr_DOTNET/Controller/UploadController.cs
+++ Enviro365Assessment_Jnr_DOTNET/Controller/UploadController.cs
@@ -17,8 +17,29 @@ public class UploadController
     public string Test() => "Hello World!";
 
     [HttpPost]
+    [Route("upload")]
     public string UploadFile(IFormFile file)
     {
         return "File uploaded successfully";
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public string GetFile(int id)
+    {
+        return "File retrieved successfully";
+    }
+
+    [HttpGet]
+    public string GetFiles()
+    {
+        return "Files retrieved successfully";
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public string DeleteFile(int id)
+    {
+        return "File deleted successfully";
     }
 }
