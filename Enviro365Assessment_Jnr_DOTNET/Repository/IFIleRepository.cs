88bd28940ee91c240d9766dec77a62ad3aeac0f0
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-22 17:08:00 +0200

    Return int (Rows affected)

--- Enviro365Assessment_Jnr_DOTNET/Repository/IFIleRepository.cs
+++ Enviro365Assessment_Jnr_DOTNET/Repository/IFIleRepository.cs
@@ -4,9 +4,9 @@ namespace Enviro365Assessment_Jnr_DOTNET.Repository;
 
 public interface IFIleRepository
 {
-    public EnvFile UploadFile(EnvFile file);
+    public int UploadFile(EnvFile file);
     public EnvFile GetFile(int id);
     public List<EnvFile> GetFiles();
-    public void DeleteFile(int id);
-    public void UpdateFile(EnvFile file);
+    public int DeleteFile(int id);
+    public int UpdateFile(EnvFile file);
 }
