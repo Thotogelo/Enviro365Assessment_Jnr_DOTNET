9d2541d98f494f5e0e6ca4857d2ddd6718a88cfd
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-22 17:04:18 +0200

    Implement repository interface

---
+++ Enviro365Assessment_Jnr_DOTNET/Repository/IFIleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Enviro365Assessment_Jnr_DOTNET.Model;
+
+namespace Enviro365Assessment_Jnr_DOTNET.Repository;
+
+public interface IFIleRepository
+{
+    public EnvFile UploadFile(EnvFile file);
+    public EnvFile GetFile(int id);
+    public List<EnvFile> GetFiles();
+    public void DeleteFile(int id);
+    public void UpdateFile(EnvFile file);
+}
