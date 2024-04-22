7d4969140ab336b176b1a7f511a028b4b9f84fbe
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-22 16:48:27 +0200

    UploadController.cs template

---
+++ Enviro365Assessment_Jnr_DOTNET/Controller/UploadController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enviro365Assessment_Jnr_DOTNET.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController
+{
+    [HttpGet]
+    public string test() => "Hello World!";
+    
+    [HttpPost]
+    public string UploadFile(IFormFile file)
+    {
+        return "File uploaded successfully";
+    }
+}
